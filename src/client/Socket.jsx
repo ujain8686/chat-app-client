8c0c02b89f4d0673eeae8312f4badd622d420607
Ujjwal <ujjwal.jain116@gmail.com>
2024-10-01 13:00:35 +0530

    Design update

--- src/client/Socket.jsx
+++ src/client/Socket.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://chat-app-server-eight-phi.vercel.app/");
 
 const Socket = () => {
   const [userID, setUserID] = useState(null);
@@ -46,69 +46,77 @@ const Socket = () => {
   }, []);
 
   return (
-    <div>
-      <form onSubmit={joinRoom}>
-        <label htmlFor="room">Enter room number: </label>
-        <br />
-        <input
-          type="number"
-          id="room"
-          onChange={(e) => setRoomNumber(e.target.value)}
-          value={roomNumber}
-          readOnly={isInRoom}
-        />
-        <button disabled={isInRoom}>{isInRoom ? "Joined" : "Join Room"}</button>
-      </form>
+    <div style={{display: "flex"}}>
+      <div style={{margin: "0px 10px"}}>
+        <h1>Welcome to Chat Rooms!! </h1>
+        <h2>Created by Ujjwal Jain</h2>
+        <form onSubmit={joinRoom}>
+          <label htmlFor="room">Enter room number: </label>
+          <br />
+          <input
+            type="number"
+            id="room"
+            onChange={(e) => setRoomNumber(e.target.value)}
+            value={roomNumber}
+            readOnly={isInRoom}
+          />
+          <button disabled={isInRoom}>
+            {isInRoom ? "Joined" : "Join Room"}
+          </button>
+        </form>
 
-      <br />
-      <br />
-      <br />
-      <form onSubmit={sendMessage}>
-        <label htmlFor="messageBox">Type your message here :</label>
         <br />
-        <input
-          type="text"
-          id="messageBox"
-          onChange={(e) => setInput(e.target.value)}
-          value={input}
-        />
-        <button disabled={!isInRoom}>Send Message</button>
-      </form>
-      <h1>Messages: </h1>
-      <br />
-      <div
-        style={{
-          border: "1px solid black",
-          display: "flex",
-          flexDirection: "column",
-          minHeight: 100,
-          height: "auto",
-          width: "600px",
-          padding: 20,
-        }}
-      >
-        {messages.map((msg) => {
-          return (
-            <p
-              style={
-                msg.userID == userID
-                  ? {
-                      color: "red",
-                      textAlign: "right",
-                      overflowWrap: "break-word",
-                    }
-                  : {
-                      color: "blue",
-                      textAlign: "left",
-                      overflowWrap: "break-word",
-                    }
-              }
-              key={Math.random()}
-            >
-              {msg.message}
-            </p>
-          );
-        })}
+        <br />
+        <br />
+        <form onSubmit={sendMessage}>
+          <label htmlFor="messageBox">Type your message here :</label>
+          <br />
+          <input
+            type="text"
+            id="messageBox"
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+          <button disabled={!isInRoom}>Send Message</button>
+        </form>
+      </div>
+      <div>
+        <h1>Messages: </h1>
+        <br />
+        <div
+          style={{
+            border: "1px solid black",
+            display: "flex",
+            flexDirection: "column",
+            minHeight: 100,
+            height: "auto",
+            width: "600px",
+            padding: 20,
+          }}
+        >
+          {messages.map((msg) => {
+            return (
+              <p
+                style={
+                  msg.userID == userID
+                    ? {
+                        color: "red",
+                        textAlign: "right",
+                        overflowWrap: "break-word",
+                      }
+                    : {
+                        color: "blue",
+                        textAlign: "left",
+                        overflowWrap: "break-word",
+                      }
+                }
+                key={Math.random()}
+              >
+                {msg.message}
+              </p>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
